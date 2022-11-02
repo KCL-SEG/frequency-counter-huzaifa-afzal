a993949669a0f1678e9821a72bece14361fb3abf
Huzaifa Afzal <muhammad.h.afzal@kcl.ac.uk>
2022-11-02 17:43:37 +0000

    "Frequencies function fixed and completed"

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        item = str(item)
+        if item not in frequencies.keys():
+            frequencies[item] = 1
+        else:
+            count = frequencies.get(item)
+            frequencies.update({item:count+1})
     return frequencies
